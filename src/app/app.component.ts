5f29223f05448ad7d3a376b398efb6723c969d04
Devang Patel <dev123@gmail.com>
2021-03-18 18:13:56 +0530

    fisrt commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import {FormGroup,FormControl,FormBuilder, Validators} from '@angular/forms'
+import { ConfirmedValidator } from './confirmedValid';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+
+
+
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'ang-dev';
+  regForm:FormGroup;
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.regForm = this.formBuilder.group({
+      fname: ['',Validators.required],
+      lname: ['',Validators.required],
+      address:  ['',Validators.required],
+      email: ['',[Validators.required,Validators.email]],
+      password: ['',Validators.required],
+      cnfpassword: ['',Validators.required],
+      zip_code: ['',[Validators.required,Validators.pattern("[0-9 ]{6}")]],
+      phone_num: ['',[Validators.required, Validators.pattern("[0-9 ]{10}")]]
+    },{
+      validator:ConfirmedValidator('password','cnfpassword')
+    })
+    // this.regForm=new FormGroup({
+    //   fname:new FormControl('',Validators.required),
+    //   lname:new FormControl('',Validators.required),
+    //   address: new FormControl('',Validators.required),
+    //   email:new FormControl('',[Validators.required,Validators.email]),
+    //   password:new FormControl('',Validators.required),
+    //   cnfpassword:new FormControl('',Validators.required),
+    //   zip_code:new FormControl('',Validators.required),
+    //   phone_num:new FormControl('',Validators.required)
+    // })
+  }
+  constructor(private formBuilder: FormBuilder) { }
+  
+ 
+  get formData(){return this.regForm.controls}
+  getData(){
+    alert("Registered")
+   console.log(this.regForm.value)
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import{ReactiveFormsModule} from'@angular/forms';
+import { FormDesignComponent } from './form-design/form-design.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormDesignComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
